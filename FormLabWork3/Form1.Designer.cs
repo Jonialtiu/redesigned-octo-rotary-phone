e811d39abbbdb29cc4c9c9cbed363148fc64adbf
Jonialtiu <71144402+Jonialtiu@users.noreply.github.com>
2021-03-07 10:57:42 +0500

    Add files via upload

---
+++ FormLabWork3/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace FormLabWork3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Edit7 = new System.Windows.Forms.Button();
+            this.Edit8 = new System.Windows.Forms.Button();
+            this.Edit9 = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.Edit4 = new System.Windows.Forms.Button();
+            this.Edit5 = new System.Windows.Forms.Button();
+            this.Edit6 = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.Edit1 = new System.Windows.Forms.Button();
+            this.Edit2 = new System.Windows.Forms.Button();
+            this.Edit3 = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.Edit0 = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.fraction = new System.Windows.Forms.Button();
+            this.sum = new System.Windows.Forms.Button();
+            this.equate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Edit7
+            // 
+            this.Edit7.Location = new System.Drawing.Point(12, 38);
+            this.Edit7.Name = "Edit7";
+            this.Edit7.Size = new System.Drawing.Size(30, 30);
+            this.Edit7.TabIndex = 7;
+            this.Edit7.Text = "7";
+            this.Edit7.UseVisualStyleBackColor = true;
+            this.Edit7.Click += new System.EventHandler(this.Edit7_Click);
+            // 
+            // Edit8
+            // 
+            this.Edit8.Location = new System.Drawing.Point(48, 38);
+            this.Edit8.Name = "Edit8";
+            this.Edit8.Size = new System.Drawing.Size(30, 30);
+            this.Edit8.TabIndex = 8;
+            this.Edit8.Text = "8";
+            this.Edit8.UseVisualStyleBackColor = true;
+            this.Edit8.Click += new System.EventHandler(this.Edit8_Click);
+            // 
+            // Edit9
+            // 
+            this.Edit9.Location = new System.Drawing.Point(84, 38);
+            this.Edit9.Name = "Edit9";
+            this.Edit9.Size = new System.Drawing.Size(30, 30);
+            this.Edit9.TabIndex = 9;
+            this.Edit9.Text = "9";
+            this.Edit9.UseVisualStyleBackColor = true;
+            this.Edit9.Click += new System.EventHandler(this.Edit9_Click);
+            // 
+            // div
+            // 
+            this.div.Location = new System.Drawing.Point(120, 38);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(30, 30);
+            this.div.TabIndex = 14;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // Edit4
+            // 
+            this.Edit4.Location = new System.Drawing.Point(12, 74);
+            this.Edit4.Name = "Edit4";
+            this.Edit4.Size = new System.Drawing.Size(30, 30);
+            this.Edit4.TabIndex = 4;
+            this.Edit4.Text = "4";
+            this.Edit4.UseVisualStyleBackColor = true;
+            this.Edit4.Click += new System.EventHandler(this.Edit4_Click);
+            // 
+            // Edit5
+            // 
+            this.Edit5.Location = new System.Drawing.Point(48, 74);
+            this.Edit5.Name = "Edit5";
+            this.Edit5.Size = new System.Drawing.Size(30, 30);
+            this.Edit5.TabIndex = 5;
+            this.Edit5.Text = "5";
+            this.Edit5.UseVisualStyleBackColor = true;
+            this.Edit5.Click += new System.EventHandler(this.Edit5_Click);
+            // 
+            // Edit6
+            // 
+            this.Edit6.Location = new System.Drawing.Point(84, 74);
+            this.Edit6.Name = "Edit6";
+            this.Edit6.Size = new System.Drawing.Size(30, 30);
+            this.Edit6.TabIndex = 6;
+            this.Edit6.Text = "6";
+            this.Edit6.UseVisualStyleBackColor = true;
+            this.Edit6.Click += new System.EventHandler(this.Edit6_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(120, 74);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(30, 30);
+            this.multiply.TabIndex = 13;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // Edit1
+            // 
+            this.Edit1.Location = new System.Drawing.Point(12, 110);
+            this.Edit1.Name = "Edit1";
+            this.Edit1.Size = new System.Drawing.Size(30, 30);
+            this.Edit1.TabIndex = 1;
+            this.Edit1.Text = "1";
+            this.Edit1.UseVisualStyleBackColor = true;
+            this.Edit1.Click += new System.EventHandler(this.Edit1_Click);
+            // 
+            // Edit2
+            // 
+            this.Edit2.Location = new System.Drawing.Point(48, 110);
+            this.Edit2.Name = "Edit2";
+            this.Edit2.Size = new System.Drawing.Size(30, 30);
+            this.Edit2.TabIndex = 2;
+            this.Edit2.Text = "2";
+            this.Edit2.UseVisualStyleBackColor = true;
+            this.Edit2.Click += new System.EventHandler(this.Edit2_Click);
+            // 
+            // Edit3
+            // 
+            this.Edit3.Location = new System.Drawing.Point(84, 110);
+            this.Edit3.Name = "Edit3";
+            this.Edit3.Size = new System.Drawing.Size(30, 30);
+            this.Edit3.TabIndex = 3;
+            this.Edit3.Text = "3";
+            this.Edit3.UseVisualStyleBackColor = true;
+            this.Edit3.Click += new System.EventHandler(this.Edit3_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.Location = new System.Drawing.Point(120, 110);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(30, 30);
+            this.subtract.TabIndex = 12;
+            this.subtract.Tag = "";
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = true;
+            this.subtract.Click += new System.EventHandler(this.subtract_Click);
+            // 
+            // Edit0
+            // 
+            this.Edit0.Location = new System.Drawing.Point(12, 146);
+            this.Edit0.Name = "Edit0";
+            this.Edit0.Size = new System.Drawing.Size(30, 30);
+            this.Edit0.TabIndex = 10;
+            this.Edit0.Text = "0";
+            this.Edit0.UseVisualStyleBackColor = true;
+            this.Edit0.Click += new System.EventHandler(this.Edit0_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(48, 146);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(30, 30);
+            this.minus.TabIndex = 15;
+            this.minus.Text = "+/-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // fraction
+            // 
+            this.fraction.Location = new System.Drawing.Point(84, 146);
+            this.fraction.Name = "fraction";
+            this.fraction.Size = new System.Drawing.Size(30, 30);
+            this.fraction.TabIndex = 16;
+            this.fraction.Text = ",";
+            this.fraction.UseVisualStyleBackColor = true;
+            this.fraction.Click += new System.EventHandler(this.fraction_Click);
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(120, 146);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(30, 30);
+            this.sum.TabIndex = 11;
+            this.sum.Text = "+";
+            this.sum.UseVisualStyleBackColor = true;
+            this.sum.Click += new System.EventHandler(this.sum_Click);
+            // 
+            // equate
+            // 
+            this.equate.Location = new System.Drawing.Point(12, 182);
+            this.equate.Name = "equate";
+            this.equate.Size = new System.Drawing.Size(138, 30);
+            this.equate.TabIndex = 17;
+            this.equate.Text = "=";
+            this.equate.UseVisualStyleBackColor = true;
+            this.equate.Click += new System.EventHandler(this.equate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(164, 221);
+            this.Controls.Add(this.equate);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.fraction);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.Edit0);
+            this.Controls.Add(this.subtract);
+            this.Controls.Add(this.Edit3);
+            this.Controls.Add(this.Edit2);
+            this.Controls.Add(this.Edit1);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.Edit6);
+            this.Controls.Add(this.Edit5);
+            this.Controls.Add(this.Edit4);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.Edit9);
+            this.Controls.Add(this.Edit8);
+            this.Controls.Add(this.Edit7);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Edit7;
+        private System.Windows.Forms.Button Edit8;
+        private System.Windows.Forms.Button Edit9;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button Edit4;
+        private System.Windows.Forms.Button Edit5;
+        private System.Windows.Forms.Button Edit6;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button Edit1;
+        private System.Windows.Forms.Button Edit2;
+        private System.Windows.Forms.Button Edit3;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.Button Edit0;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button fraction;
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.Button equate;
+    }
+}
+
